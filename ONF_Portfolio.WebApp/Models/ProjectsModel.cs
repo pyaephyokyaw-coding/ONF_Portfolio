966eb15a7fecf8da053d1c111a3d41a5ef49d7b6
ppk <pyaephyokyaw.dev@gmail.com>
2025-06-20 22:50:06 +0630

    add ProjectsModel

---
+++ ONF_Portfolio.WebApp/Models/ProjectsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ONF_Portfolio.WebApp.Models;
+
+public class ProjectsModel
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Url { get; set; } = string.Empty;
+
+    public string CreatedDate { get; set; } = DateTime.Now.ToString("yyyy-MM-dd");
+}
